c52b72e74a645a118565af90c91cc076f86471c8
Olivia Ifebuche <oliviaifebuche002@gmail.com>
2026-02-23 16:13:30 -0800

    weather app

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import SearchBar from "./components/SearchBar"
+import axios from "axios";
+import ForCast from "./components/ForCast";
+import { BiLoaderCircle } from "react-icons/bi";
+
+
+function App() {
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+  const fetchData = async (city)=>{
+    setLoading(true);
+    setError("");
+    try{
+      const url= `${API_URL}?q=${city}&units=metric&appid=${API_KEY}`;
+      const response = await axios.get(url);
+      console.log(response.data);
+      setWeather(response.data);
+    } catch(err){
+      if(err.response && err.response.status === 404){
+        setError("City not found, please enter another city!")
+      } else{
+        setError("An error occurred, please try again later.")
+      }
+      setWeather(null);
+    } finally{
+      setLoading(false);
+    }
+  }
+  // useEffect(()=>{
+  //     fetchData("New York");
+  // }, [city]);
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[url(/background.jpg)] bg-cover bg-center overflow-hidden">
+      <div className=" bg-black/20 text-white p-8 max-w-md w-full rounded-lg shadow-xl">
+        <h1 className="text-3xl font-bold text-center pb-6 text-pink-500">Weather App</h1>
+        <SearchBar fetchData={fetchData} />
+        {loading && <p className="text-center mt-4 text-lg"><BiLoaderCircle className="animate-spin mx-auto" size={30} /></p>}
+        {error && <p className="text-center mt-4 text-lg text-red-500">{error}</p>}
+        {weather && <ForCast weather={weather} />}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+import { CiSearch } from 'react-icons/ci'
+
+const SearchBar = ({fetchData}) => {
+     const [city, setCity] = useState("")
+    const handleChange = (e) => setCity(e.target.value);
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(city){
+            fetchData(city);
+            setCity("");
+        }
+    }
+    return (
+        <form className='flex' onSubmit={handleSubmit}>
+            <input type="text" placeholder='Search City...'
+                value={city}
+                onChange={handleChange} className='p-3 outline-none rounded-l-lg text-2xl flex-1 placeholder:text-pink-400 placeholder:text-xl placeholder:font-semibold' />
+            <button type='submit'><CiSearch className='inline font-extrabold text-6xl bg-pink-400 text-white p-3 rounded-r-lg pointer hover:bg-pink-600' /></button>
+        </form>
+    )
+}
+
+export default SearchBar
